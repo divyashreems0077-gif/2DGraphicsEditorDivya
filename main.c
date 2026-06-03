9a6efbcc4867974e3a552d3ad3bc86caeff002de
divyashreems0077-gif <divyashreems0077@gmail.com>
2026-06-03 16:55:31 +0530

    Day 2 - Draw point feature

--- main.c
+++ main.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 
-char canvas[20][40];
+char canvas[10][20];
 
 void initializeCanvas()
 {
-    int i,j;
+    int i, j;
 
-    for(i=0;i<20;i++)
+    for(i = 0; i < 10; i++)
     {
-        for(j=0;j<40;j++)
+        for(j = 0; j < 20; j++)
         {
-            canvas[i][j]='_';
+            canvas[i][j] = '_';
         }
+    }
+}
+
+void drawPoint(int row, int col)
+{
+    if(row >= 0 && row < 10 && col >= 0 && col < 20)
+    {
+        canvas[row][col] = '*';
     }
 }
 
 void displayCanvas()
 {
-    int i,j;
+    int i, j;
 
-    for(i=0;i<20;i++)
+    for(i = 0; i < 10; i++)
     {
-        for(j=0;j<40;j++)
+        for(j = 0; j < 20; j++)
         {
-            printf("%c",canvas[i][j]);
+            printf("%c", canvas[i][j]);
         }
         printf("\n");
     }
@@ -31,7 +39,18 @@ void displayCanvas()
 
 int main()
 {
+    int row, col;
+
     initializeCanvas();
+
+    printf("Enter row (0-9): ");
+    scanf("%d", &row);
+
+    printf("Enter column (0-19): ");
+    scanf("%d", &col);
+
+    drawPoint(row, col);
+
     displayCanvas();
 
     return 0;
